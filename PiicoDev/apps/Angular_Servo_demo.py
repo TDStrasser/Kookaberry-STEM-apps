09c686816473d4fd124329fca7ade13aa0fd5335
TDStrasser <43877585+TDStrasser@users.noreply.github.com>
2026-07-04 13:40:23 +1000

    Updated example servo scripts

--- PiicoDev/apps/Angular_Servo_demo.py
+++ PiicoDev/apps/Angular_Servo_demo.py
@@ -5,18 +5,18 @@ from PiicoDev_Servo_Kookaberry import PiicoDev_Servo, PiicoDev_Servo_Driver
 
 i2c = SoftI2C(scl=Pin('P3A'), sda=Pin('P3B'))
 controller = PiicoDev_Servo_Driver(i2c)
-servo_1 = PiicoDev_Servo(controller, 1)
-servo_4 = PiicoDev_Servo(controller, 4)
+servo_1 = PiicoDev_Servo(controller, 1,min_us=600, max_us=2600, degrees=180)
+# Note SG90 servos seem to range from 600usec (90 deg) to 2600usec (180 deg) pulse widths
 
-angles = [0,45,90,135,180,135,90,45,0]
+angles = [0,45,90,135,180,135,90,45,0,90]
 # Step the servos
 for angle in angles:
   servo_1.angle = angle
-  servo_4.angle = angle
   sleep_ms(1000)
 
 # Sweep the servo slowly 0->180°
 for x in range(0,180,5):
     servo_1.angle = x
-    servo_4.angle = x
     sleep_ms(40)
+# Send servo to the middle (90 degrees)
+servo_1.angle = 90
--- PiicoDev/apps/Continuous_Rotation_Servo_demo.py
+++ PiicoDev/apps/Continuous_Rotation_Servo_demo.py
@@ -6,9 +6,19 @@ from PiicoDev_Servo_Kookaberry import PiicoDev_Servo, PiicoDev_Servo_Driver
 i2c = SoftI2C(scl=Pin('P3A'), sda=Pin('P3B'))
 controller = PiicoDev_Servo_Driver(i2c)
 
-servo_2 = PiicoDev_Servo(controller, 2, midpoint_us=1500, range_us=1800) # Connect a 360° servo to channel 2
+servo_2 = PiicoDev_Servo(controller, 2, midpoint_us=1500, range_us=800) # Connect a 360° servo to channel 2
+# Note the FS90R continuous servo has a pulse range of:
+#  700usec (max negative speed) to 2300usec (max positive speeed) 
+#  1500usec is the zero speed point.
+
+def ramp(servo,s1,s2):
+  for s in range(s1,s2,int((s2-s1)/abs(s2-s1))):
+    servo.speed = s/10
+    print(s/10)
+    sleep_ms(1000)
 
 # Ramp the servo slowly through its range
-for s in range(-10,10,2):
-    servo_2.speed = s/10
-    sleep_ms(1000)
+ramp(servo_2,0,-10)
+ramp(servo_2,-10,10)
+ramp(servo_2,10,-1)
+servo_2.release()
