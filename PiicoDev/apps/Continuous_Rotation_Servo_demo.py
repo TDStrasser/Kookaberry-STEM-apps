de35dc27af5d2ffed981f3252ea823dda762bcc6
TDStrasser <43877585+TDStrasser@users.noreply.github.com>
2026-07-04 14:47:59 +1000

    Improved the continuous rotation servo example

--- PiicoDev/apps/Continuous_Rotation_Servo_demo.py
+++ PiicoDev/apps/Continuous_Rotation_Servo_demo.py
@@ -11,14 +11,23 @@ servo_2 = PiicoDev_Servo(controller, 2, midpoint_us=1500, range_us=800) # Connec
 #  700usec (max negative speed) to 2300usec (max positive speeed) 
 #  1500usec is the zero speed point.
 
-def ramp(servo,s1,s2):
-  for s in range(s1,s2,int((s2-s1)/abs(s2-s1))):
-    servo.speed = s/10
-    print(s/10)
-    sleep_ms(1000)
+# Speeds in this script are in the range -100 to +100
+current_speed = 0 #
 
-# Ramp the servo slowly through its range
-ramp(servo_2,0,-10)
-ramp(servo_2,-10,10)
-ramp(servo_2,10,-1)
-servo_2.release()
+def ramp(servo,s1,s2):
+  if s2 == s1: return # Skip zero changes
+  direction = int((s2-s1)/abs(s2-s1))
+  for s in range(s1,s2+direction,direction):
+    servo.speed = s/100
+    sleep_ms(20)
+
+speeds = [0,-100,100,0,-50,50,0,-25,25,0] # Put the speeds you want in this array (-100 to +100)
+# Step the servos
+for speed in speeds:
+  if speed < -100: speed = -100 # limit out of range
+  if speed > 100: speed = 100
+  ramp(servo_2,current_speed, speed)
+  current_speed = speed # Record the last target speed
+  sleep_ms(1000) # Stay at the sppeed for a while
+
+servo_2.release() # Stop the servo driver PWM so it doesnt creep
